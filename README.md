a8fc3482bc0b3ace54764b21f2ac1abe068d7cd0
Abu Sahil <abusa16198@gmail.com>
2026-06-01 16:15:59 +0530

    day 1 of my jour

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Date : 31/05/2026
 End  : .................. (target: 8/06/2026)
+progress day 01
+=> I was done a big mistake in my earlier coding journy b/c of that my code do not get submmited 
+<input type="submit" value="Submit> not <input type="button" value="Submit>
--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-from flask import Flask
+from flask import Flask , render_template,request
+
+
 app = Flask(__name__)
 
+@app.route('/', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form['email']
+        return f"Data submitted successfully! Name: {name} and Email: {email}"
+    else:
+        return render_template('login.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
